92023cdca0052a16a5e277bc73b6a3db1de3b3a5
RenRen62 <nakayama.r@x-point-1.net>
2024-05-16 15:10:15 +0900

    feat: :sparkles: prismaの時差補正処理を追加

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,50 @@
+/* eslint-disable 
+ functional/no-return-void, 
+ functional/no-loop-statements, 
+ functional/no-conditional-statements,
+ functional/immutable-data,
+ functional/no-expression-statements,
+ functional/functional-parameters,
+ @typescript-eslint/no-explicit-any
+ */
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
-export type Prisma = PrismaClient;
+// オフセット時間を設定するヘルパー関数
+const setOffsetTime = (object: any, offsetTime: number) => {
+  if (object === null || typeof object !== "object") return;
+
+  for (const key of Object.keys(object)) {
+    const value = object[key];
+    if (value instanceof Date) {
+      object[key] = new Date(value.getTime() + offsetTime);
+    } else if (value !== null && typeof value === "object") {
+      setOffsetTime(value, offsetTime);
+    }
+  }
+};
+
+// UTCの時間をJSTに変換(+09:00)してDBに保存、またレコード取得時にも時差を補正(-09:00)
+const adjustTimeExtension = (prisma: PrismaClient) => {
+  const offsetTime = 9 * 60 * 60 * 1000; // JSTのオフセット時間(UTC+09:00)
+
+  return prisma.$extends({
+    query: {
+      $allOperations: async ({ args, query }) => {
+        // DBへの保存時にUTCからJSTに変換
+        setOffsetTime(args, offsetTime);
+
+        const result = await query(args);
+
+        // DBからの取得時にJSTからUTCに変換
+        setOffsetTime(result, -offsetTime);
+
+        return result;
+      },
+    },
+  });
+};
+
+// prismaインスタンスをadjustTimeでラップ
+const prisma = adjustTimeExtension(new PrismaClient());
+
+export { prisma, PrismaClient as Prisma };
