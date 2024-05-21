6a897ebea64c67337978aea31da801c9402777f1
RenRen62 <nakayama.r@x-point-1.net>
2024-05-21 12:18:26 +0900

    fix: :lady_beetle: エラークラス修正

--- src/utils/exceptions.ts
+++ src/utils/exceptions.ts
@@ -4,23 +4,27 @@ functional/no-classes,
 functional/no-expression-statements
 */
 
-export class BadRequestException extends Error {
-  constructor(message: string) {
+export class HTTPException extends Error {
+  public statusCode: number;
+  public error: string;
+
+  constructor(statusCode: number, error: string, message: string) {
     super(message);
+    this.statusCode = statusCode;
+    this.error = error;
+    Object.setPrototypeOf(this, HTTPException.prototype);
+  }
+}
+
+export class BadRequestException extends HTTPException {
+  constructor(message: string) {
+    super(400, "Bad Request", message);
     Object.setPrototypeOf(this, BadRequestException.prototype);
   }
 }
-
-export class NotFoundException extends Error {
+export class NotFoundException extends HTTPException {
   constructor(message: string) {
-    super(message);
+    super(404, "Not Found", message);
     Object.setPrototypeOf(this, NotFoundException.prototype);
-  }
-}
-
-export class InternalServerError extends Error {
-  constructor(message: string) {
-    super(message);
-    Object.setPrototypeOf(this, InternalServerError.prototype);
   }
 }
